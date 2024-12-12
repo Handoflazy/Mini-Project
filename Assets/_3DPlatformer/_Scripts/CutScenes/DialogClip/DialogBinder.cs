8579fc16d1a148b68dbd7d300dd7b7a294056bf9
Đoàn Thành Cao Nguyên <2151120042@ut.edu.vn>
2024-12-12 16:34:25 +0700

    Base Custom timeline track and clip for cutscene dialogue

---
+++ Assets/_3DPlatformer/_Scripts/CutScenes/DialogClip/DialogBinder.cs
@@ -0,0 +1,7 @@
+namespace Platformer.CutScenes.DialogClip
+{
+    public abstract class DialogBinder : UnityEngine.Component
+    {
+        public abstract void SetDialog(string dialogLineID);
+    }
+}
